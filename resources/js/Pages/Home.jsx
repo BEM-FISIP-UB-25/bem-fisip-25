102a5b80f5c5df5bb057f924ed5f958a97039c08
AiFahri <fahrinurilll@gmail.com>
2025-09-16 00:16:52 +0700

    initial commit

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Head } from "@inertiajs/react";
+
+export default function Hello() {
+    return (
+        <>
+            <Head title="Hello" />
+            <div>
+                <h1 className="text-red-600 text-8xl">Home ni bos</h1>
+            </div>
+        </>
+    );
+}
