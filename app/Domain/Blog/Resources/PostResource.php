75b1c12f368d9ba25e1ec7d03012ba9eaf4c9c5b
AiFahri <fahrinurilll@gmail.com>
2025-09-20 18:42:02 +0700

    feat(blog): create PostResource for transforming blog post data

---
+++ app/Domain/Blog/Resources/PostResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Domain\Blog\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PostResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'slug' => $this->slug,
+            'abstract' => $this->abstract,
+            'image_url' => $this->image_url,
+            'external_url' => $this->external_url,
+            'published_at' => $this->published_at?->format('d M Y'),
+            'status' => $this->status,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
+
